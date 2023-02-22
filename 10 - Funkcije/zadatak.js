2acf2410ee6d64d82e0480cee193979b788ccb07
Vedran Stojnović <phidrho@gmail.com>
2023-02-22 19:13:21 +0100

    zadatak email gotov

--- 10 - Funkcije/zadatak.js
+++ 10 - Funkcije/zadatak.js
@@ -1,3 +1,5 @@
+// ZADATAK 1 - najduža riječ iz popisa
+
 function najduzaRijecIBrojSlova(popis) {
     let brojSlova = 0;
     let najduzaRijec = "";
@@ -15,3 +17,37 @@ function najduzaRijecIBrojSlova(popis) {
 
 mojPopis = ["Jabuka", "Kruška", "Šljiva", "Lubenica", "Jabukova"];
 console.log("Najduza rijec je:", najduzaRijecIBrojSlova(mojPopis)[0], "i ima ukupno", najduzaRijecIBrojSlova(mojPopis)[1], "slova.");
+
+// ZADATAK 2 - provjera radi li se o email adresi
+
+function checkEmail(emailAdresa) {
+    var ostatakTeksta = "";
+    var mankiPostoji = 0;
+    var tockaPostoji = 0;
+
+
+    if (emailAdresa.indexOf("@") === -1) {
+        mankiPostoji = 0;
+        console.log("manki ne postoji");
+    } else {
+        mankiPostoji = 1;
+        console.log("manki postoji");
+        ostatakTeksta = emailAdresa.slice(emailAdresa.indexOf("@") + 1);
+        if (ostatakTeksta.indexOf(".") === -1) {
+            console.log("tocka ne postoji");
+            tockaPostoji = 0;
+        } else {
+            console.log("tocka postoji");
+            tockaPostoji = 1;
+        }
+    }
+
+    if (mankiPostoji && tockaPostoji) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var isEmailOk = checkEmail("mojeime@emailcom");
+console.log("email ok = " + isEmailOk);
