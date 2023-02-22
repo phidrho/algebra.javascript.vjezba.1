916ab34065aeaf17ff5e5899ceb6bef11d6518d9
Vedran Stojnović <phidrho@gmail.com>
2023-02-22 18:09:17 +0100

    dorađen zadatak

--- 10 - Funkcije/zadatak.js
+++ 10 - Funkcije/zadatak.js
@@ -1,16 +1,21 @@
 function najduzaRijecIBrojSlova(popis) {
     let brojSlova = 0;
     let najduzaRijec = "";
-    let duljinaPopisa = popis.length;
+    let duljinaPopisa = popis.length; // ovo je možda "anonymous" funkcija ili je to je svojstvo - property - pa ne treba zagrade za poziv
     for (i = 0; i < duljinaPopisa; i++) {
+        if (popis[i].length == najduzaRijec.length) {
+            najduzaRijec = popis[i] + ", " + najduzaRijec;
+        } else {
+            najduzaRijec = popis[i];
+        }
         if (popis[i].length > brojSlova) {
             brojSlova = popis[i].length;
-            najduzaRijec = popis[i]; 
+
         }
     }
     return [najduzaRijec, brojSlova];
 }
 
 
-mojPopis = ["Jabuka", "Kruška", "Šljiva", "Lubenica"];
-console.log("Najduza rijec je:", najduzaRijecIBrojSlova(mojPopis)[0], "i ima ukupno", najduzaRijecIBrojSlova(mojPopis)[1], "slova.");
+mojPopis = ["Jabuka", "Kruška", "Šljiva", "Lubenica", "Jabukova"];
+console.log("Najduza/e rijec/i je:", najduzaRijecIBrojSlova(mojPopis)[0], "i ima ukupno", najduzaRijecIBrojSlova(mojPopis)[1], "slova.");
