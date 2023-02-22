ce145346fb917daa6efd4dd5be5f6042d07a2a7d
Vedran Stojnović <phidrho@gmail.com>
2023-02-22 18:19:08 +0100

    korekcija jer ne radi dobro za više riječi

--- 10 - Funkcije/zadatak.js
+++ 10 - Funkcije/zadatak.js
@@ -3,19 +3,15 @@ function najduzaRijecIBrojSlova(popis) {
     let najduzaRijec = "";
     let duljinaPopisa = popis.length; // ovo je svojstvo - property - pa ne treba zagrade za poziv
     for (i = 0; i < duljinaPopisa; i++) {
-        if (popis[i].length == najduzaRijec.length) {
-            najduzaRijec = popis[i] + ", " + najduzaRijec;
-        } else {
+        if (popis[i].length > najduzaRijec.length) {
             najduzaRijec = popis[i];
         }
-        if (popis[i].length > brojSlova) {
-            brojSlova = popis[i].length;
-
-        }
     }
+
+    brojSlova = najduzaRijec.length;
     return [najduzaRijec, brojSlova];
 }
 
 
 mojPopis = ["Jabuka", "Kruška", "Šljiva", "Lubenica", "Jabukova"];
-console.log("Najduza/e rijec/i je/su:", najduzaRijecIBrojSlova(mojPopis)[0], "i ima ukupno", najduzaRijecIBrojSlova(mojPopis)[1], "slova.");
+console.log("Najduza rijec je:", najduzaRijecIBrojSlova(mojPopis)[0], "i ima ukupno", najduzaRijecIBrojSlova(mojPopis)[1], "slova.");
