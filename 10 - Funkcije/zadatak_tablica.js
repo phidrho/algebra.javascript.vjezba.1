ac72231a20a0e654f6be10c18dc5a6cead6b416a
Vedran Stojnović <phidrho@gmail.com>
2023-03-02 16:59:08 +0100

    zadatak za zadacu

---
+++ 10 - Funkcije/zadatak_tablica.js
@@ -0,0 +1,12 @@
+
+console.log(`TABLICA MNOŽENJA:`)
+
+for (var i = 1; i < 11; i++) {
+    for (var j = 1; j < 11; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log(`============`)
+}
+    
+
+    
