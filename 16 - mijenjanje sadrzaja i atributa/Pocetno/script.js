0db81ce61f7301db7f60c0a4d45a22319a3f9ec3
Vedran Stojnović <phidrho@gmail.com>
2023-03-06 17:32:27 +0100

    rjesenje zadatak 2

---
+++ 16 - mijenjanje sadrzaja i atributa/Pocetno/script.js
@@ -0,0 +1,29 @@
+let mojH1 = document.querySelector("h1");
+console.log(mojH1);
+console.log(mojH1.textContent);
+
+mojH1.innerText = 'Karlovac';
+
+let spanUH3 = document.querySelector(".description span");
+
+console.log(spanUH3);
+console.log(spanUH3.textContent);
+
+spanUH3.outerHTML = '<h3>5 rijeka</h3>';
+
+let divInfo = document.querySelector(".info");
+console.log(divInfo);
+console.log(divInfo.textContent);
+divInfo.innerText = 'Peta rijeka je pivo.';
+
+let footerUl = document.getElementById("footer").firstElementChild;
+console.log(footerUl);
+console.log(footerUl.textContent);
+console.log(footerUl.children);
+let footerUlLi = footerUl.children;
+
+var rijeke = ['Dobra', 'Korana', 'Kupa', 'Mrežnica', 'Karlovačko'];
+
+for (let i = 0; i < footerUlLi.length; i++) {
+    footerUlLi[i].innerText = rijeke[i];
+}
