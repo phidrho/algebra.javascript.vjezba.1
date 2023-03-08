d9b04bac3435f8975a1f4fc2ff8655160dc81229
Vedran Stojnović <phidrho@gmail.com>
2023-03-08 18:36:57 +0100

    napredne funkcije zad 1 i 2

--- 20 - Funkcije - napredno/scripts.js
+++ 20 - Funkcije - napredno/scripts.js
@@ -1,4 +1,4 @@
-// 1.
+// zadatak 1
 var x = "John Doe"; 
 function vratiNesto(name) {
    var x = name;
@@ -6,4 +6,20 @@ function vratiNesto(name) {
    return f;
 }
 var y = vratiNesto("Jane Doe");
+console.log(y);
+
 var z = vratiNesto()();
+console.log(z);
+console.log(vratiNesto("Goran")());
+
+
+// zadatak 2
+var nase_ime = 'neko ime';
+
+(function vratiNesto2(name) {
+   var x = name;
+   function f() {
+      return x;
+   };
+   return f;
+})(nase_ime);
