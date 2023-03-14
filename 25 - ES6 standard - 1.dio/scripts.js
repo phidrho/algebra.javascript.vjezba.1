c0f3848904f6a4867ada012b659cbddccc195f02
Vedran Stojnovic <phidrho@gmail.com>
2023-03-14 18:08:09 +0100

    prvi zadatak - korekcija koda sa use strict

---
+++ 25 - ES6 standard - 1.dio/scripts.js
@@ -0,0 +1,27 @@
+'use strict';
+// 1.
+//a = 78;
+var a = 78;
+
+//var b = 034;
+var b = 34;
+
+//delete a;
+//delete window.b;
+var myObject = {property1: 1, property2: 2, property1: 1};
+
+// 2.
+const users = [
+    {name: 'Fabio', surname: 'Biondi'},
+    {name: 'Mario', surname: 'Rossi'},
+];
+const user = {name: 'Lorenzo', surname: 'Verdi'};
+
+// 3.
+for(let i = 0; i < 10; i++){
+    setTimeout(function(){
+        console.log('The number is ' + i);
+    }, 1000);
+}
+
+//console.log(i);
